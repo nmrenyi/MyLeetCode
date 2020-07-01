f1d9cbd58a911c4fd45213c1a6eb2b119c53fbf6
renyi <nmrenyi@outlook.com>
2020-07-01 08:28:05 +0800

    ry add leetcode80

---
+++ Tags/Array/80-RemoveDuplicatesfromSortedArrayII.cpp
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+// My Solution, use stl functions 
+// But the erase function in vector may bring low efficiency, O(n^2) in worst case
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() <= 1) {
+            return nums.size();
+        }
+        int cnt = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i - 1] == nums[i]) {
+                cnt++;
+            } else {
+                cnt = 1;
+            }
+            if (cnt > 2) {
+                nums.erase(nums.begin() + i, nums.begin() + i + 1);
+                i--;
+            }
+        }
+        return nums.size();
+    }
+};
+
+// A more subtle solution, use the property that the array is sorted,
+// Only consider the nums[i - 2] and num, i is the place which is ready to be covered 
+// O(n) complexity
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0;
+        for (int num : nums) {
+            if (i < 2 || num > nums[i - 2]) {
+                nums[i++] = num;
+            }
+        }
+        return i;
+    }
+};
